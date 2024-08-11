89c3a6ae976b3a66bff17b0d1f301f3f06a3c45e
Suheer Zahid <150055579+CodeWithSuheer@users.noreply.github.com>
2024-08-11 17:59:54 +0500

    first commit

---
+++ src/page/home/HeroSection.tsx
@@ -0,0 +1,11 @@
+const HeroSection = () => {
+  return (
+    <>
+      <section className="w-full">
+        <div className="">Suheer</div>
+      </section>
+    </>
+  );
+};
+
+export default HeroSection;
